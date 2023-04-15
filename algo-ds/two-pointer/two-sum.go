f2192f1f0d20722751630a7b476e8331a793fcf0
Anh Tuan Nguyen <anh.tuan.nguyen@skymavis.com>
2023-04-15 14:20:44 +0700

    correct twosum problem

--- algo-ds/two-pointer/two-sum.go
+++ algo-ds/two-pointer/two-sum.go
@@ -1,21 +1,21 @@
 package main
 
-func twoSum(numbers []int, target int) []int {
-	l, r := 0, len(numbers)-1
-	for l < r {
-		sum := numbers[l] + numbers[r]
-		if sum == target {
-			return []int{l + 1, r + 1}
-		} else if sum > target {
-			r--
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
 		} else {
-			l++
+			m[v] = i
 		}
+
 	}
 	return []int{}
 }
 
 func main() {
 	nums := []int{0, 1, 0, 2, 3}
-	twoSum(nums, 3)
+	fmt.Println(twoSum(nums, 3))
 }
